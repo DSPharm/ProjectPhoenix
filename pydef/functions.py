776ba9b83f04c9aba5658f80c6577c8e5f84190e
DSPharm <eugenrusu.serviciu@gmail.com>
2026-06-02 15:47:19 +0300

    Am adaugat functia de gradul 2

--- pydef/functions.py
+++ pydef/functions.py
@@ -1,5 +1,30 @@
+import math
+
 def cub(x):
     return x**3
+
+def patrat(x):
+    return x**2
+
+def ecuatie_gradul_2(a, b, c):
+    if a == 0:
+        return "Nu este ecuatie de gradul 2"
+    
+    delta = b**2 - 4*a*c
+
+    if delta < 0:
+        return "Nu are solutii reale"
+    
+    elif delta == 0:
+        x = -b/(2*a)
+        return(x,)
+    
+    else:
+        x1 = (-b + math.sqrt(delta))/(2*a)
+        x2 = (-b - math.sqrt(delta))/(2*a)  
+        return(x1, x2)
+    
+print(ecuatie_gradul_2(3,-1,-2))
 
 
 lista = []
@@ -15,3 +40,6 @@ x = 1
 for i in lista:
     print("Cubul numarului", x, ": ", cub(i))
     x +=1
+
+for z in range(len(lista)):
+    print (z+1)
